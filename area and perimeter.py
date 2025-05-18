e20815397c906144d4ee94bdb1a9f86ac0adc1d7
IshitaGupta2004 <2304281530031@kashiit.ac.in>
2025-05-18 23:35:23 +0530

    Add files via upload

---
+++ area and perimeter.py
@@ -0,0 +1,15 @@
+print("Hello, I’m Ishita Gupta ! Nice to meet you.")
+
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+
+area = length * width
+perimeter = 2 * (length + width)
+
+
+print(f"Length : {length}")
+print(f"Width  : {width}")
+print(f"Area = {area}")
+print(f"Perimeter = {perimeter}")
